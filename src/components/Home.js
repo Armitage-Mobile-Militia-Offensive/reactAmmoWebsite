5eaf1fecc3ccff29a041a0b6c2655b22468fe21c
Short_Circuit <shortcircuit1024@gmail.com>
2017-03-16 23:38:48 -0500

    Updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {connectProfile} from '../auth';
 import './Home.css';
 
+import ClockUTC from './ClockUTC';
+
+
 class Home extends Component {
   static propTypes = {
     ...connectProfile.PropTypes
@@ -15,6 +18,7 @@ class Home extends Component {
             <h3 className="landingHeader display-4 text-center" style={{ fontFamily: 'Electrolize', textAlign: 'center', marginTop: '25%', color: 'white'}}>Armitage Mobile Militia Offensive</h3>
             <h1 className="landingHeader display-3 text-center" style={{ fontFamily: 'Electrolize', textAlign: 'center', marginBottom: '0%', color: '#F59B45'}}><b> A . M . M . O . </b></h1>
             <p className="landingText text-center" style={{ fontFamily: 'Offside', textAlign: 'center'}}><small>ex oblivione, unitas</small></p>
+            <ClockUTC/>
           </span>
         </div>
       </div>
